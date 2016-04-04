10606ebdb98709a18512fefa23a61dd89f5d204e
Laoujin <woutervs@hotmail.com>
2016-04-05 00:30:05 +0200

    css cleanup

--- src/components/App/Header.js
+++ src/components/App/Header.js
@@ -1,5 +1,4 @@
 import React, { Component, PropTypes } from 'react';
-import Navigation from './Navigation.js';
 import CurrentTemperature from '../Temperature/CurrentTemp.js';
 
 export default class Header extends Component {
@@ -9,10 +8,11 @@ export default class Header extends Component {
 
   render() {
     return (
-      <div className="Header" style={{height: 47}}>
-        <img className="Header-brandImg" src={require('./logo-small.png')} width="38" height="38" alt="React" />
-        <span className="Header-brandTxt">Delicious Raspberry Pie</span>
-        <Navigation className="Header-nav" />
+      <div style={{height: 47, backgroundColor: '#373277', color: '#fff'}}>
+        <div style={{padding: 5, display: 'inline-block'}}>
+          <img src={require('./logo-small.png')} width="38" height="38" />
+          <strong style={{marginLeft: 20}}>Delicious Raspberry Pie</strong>
+        </div>
         <div className="label label-danger" style={{display: 'inline-block', margin: 10, float: 'right', borderRadius: '1em', fontSize: 18}}>
           <CurrentTemperature temp={this.props.temp} />
         </div>
